e6c851ea08b8032a5a94a78596cedf444726da3b
Douglas Russell <douglas.russell7@btinternet.com>
2022-01-21 08:51:07 +0000

    adds tests for Guitar

---
+++ src/test/java/instrumentTest/stringTest/GuitarTest.java
@@ -0,0 +1,62 @@
+package instrumentTest.stringTest;
+
+import instrument.InstrumentType;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GuitarTest {
+    private Guitar guitar;
+
+    @Before
+    public void before() {
+        guitar = new Guitar("Schecter", "C-7", "Mahogany", 674.50, 1349.00, 7);
+    }
+
+    @Test
+    public void hasMake() {
+        assertEquals("Schecter", guitar.getMake());
+    }
+
+    @Test
+    public void hasModel() {
+        assertEquals("C-7", guitar.getModel());
+    }
+
+    @Test
+    public void hasType() {
+        assertEquals(InstrumentType.STRING, guitar.getType());
+    }
+
+    @Test
+    public void hasMaterial() {
+        assertEquals("Mahogany", guitar.getMaterial());
+    }
+
+    @Test
+    public void hasPurchasePrice() {
+        assertEquals(674.50, guitar.getPurchasePrice(), 0.0);
+    }
+
+    @Test
+    public void hasSalePrice() {
+        assertEquals(1349.00, guitar.getSalePrice(), 0.0);
+    }
+
+    @Test
+    public void hasNumberOfStrings() {
+        assertEquals(7, guitar.getNumberOfStrings());
+    }
+
+    @Test
+    public void canPlay() {
+        assertEquals("No Stairway, denied", guitar.play());
+    }
+
+    @Test
+    public void canCalculateMarkup() {
+        assertEquals(674.50, guitar.calculateMarkup(), 0.0);
+    }
+}
+
